09b6897d64e3f76411d198c30969d81fb7fc69a0
rimbee <145882608+rimbee@users.noreply.github.com>
2025-03-27 22:26:23 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+function openModal(src) {
+    const modal = document.getElementById("modal");
+    const modalImg = document.getElementById("modal-img");
+    modal.style.display = "flex"; 
+    modalImg.src = src;
+}
+
 function closeModal() {
     const modal = document.getElementById("modal");
     modal.style.display = "none";
